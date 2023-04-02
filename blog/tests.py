205ec9ef6b40fe643aa27468be298bb6a3db9111
Asylbek Turdaliev <turdalievasyl@gmail.com>
2023-04-02 17:55:29 +0600

    дз2

--- blog/tests.py
+++ blog/tests.py
@@ -9,5 +9,23 @@ class HelloViewTextCase(TestCase):
     def test_hello(self):
         response = self.client.get(reverse("hello-view"))  
         excepted_data = "Hello"
-        self.assertEqual(excepted_data, response.context.decode())
+        self.assertEqual(excepted_data, response.content.decode())
         self.assertEqual(500, response.status_code)
+
+class AboutViewTestCase(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def test_about(self):
+        response = self.client.get(reverse('about-views'))
+        excepted_data = "Какая-нибудь строка 2"
+        self.assertEqual(excepted_data, response.content.decode())
+
+class ContactsViewTestCase(TestCase):
+    def setup(self):
+        self.client = Client()
+    
+    def test_contacts(self):
+        response = self.client.get(reverse('contacts-views'))
+        excepted_data = 'Какая-нибудь строка'
+        self.assertEqual(excepted_data, response.content.decode())
