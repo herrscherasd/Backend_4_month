e7a4cf7e4a082cf3467fef5db23ca25d2c3a0d3d
Asylbek Turdaliev <turdalievasyl@gmail.com>
2023-04-21 23:01:40 +0600

    hm7

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from users.views import register
+from users.views import RegisterView
 
 urlpatterns = [
-    path("users/register/", register, name="register")
+    path("users/register/", RegisterView.as_view(), name="register")
 ]
--- users/views.py
+++ users/views.py
@@ -1,17 +1,32 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views import generic
 from users.forms import UserRegistrationForm
 
 # Create your views here.
 
-def register(request):
-    if request.method == "POST":
-        user_form = UserRegistrationForm(request.POST)
-        if user_form.is_valid():
-            new_user = user_form.save(commit=False)
-            new_user.set_password(user_form.clean_password2())
-            new_user.save()
-            return render(request, "registration/register_done.html", {'user' : new_user})
+# def register(request):
+#     if request.method == "POST":
+#         user_form = UserRegistrationForm(request.POST)
+#         if user_form.is_valid():
+#             new_user = user_form.save(commit=False)
+#             new_user.set_password(user_form.clean_password2())
+#             new_user.save()
+#             return render(request, "registration/register_done.html", {'user' : new_user})
     
-    else:
-        user_form = UserRegistrationForm()
-    return render(request, 'registration/register.html', {"form" : user_form})
+#     else:
+#         user_form = UserRegistrationForm()
+#     return render(request, 'registration/register.html', {"form" : user_form})
+
+
+class RegisterView(generic.FormView):
+    form_class=UserRegistrationForm
+    success_url=reverse_lazy('register-done')
+    template_name='registration/register.html'
+
+    def form_valid(self, form):
+        new_user = form.save(commit=False)
+        new_user.set_password(form.cleaned_data['password2'])
+        new_user.save()
+        return super().form_vaild(form)
+    
